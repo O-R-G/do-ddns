2ffa700b77c33953c9ddf26cb73ee83852635408
david reinfurt <reinfurt@o-r-g.com>
2020-06-30 13:05:48 -0400

    use environmental variables, add ip.txt, only update when ip has chnaged

--- update-do-ddns.sh
+++ update-do-ddns.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
-ACCESS_TOKEN=thistoken
-DOMAIN=this.com
-# () allow multiple ids (RECORD_ID_1 RECORD_ID_2 RECORD_ID_n)
-RECORD_IDS=()
+# requires ENV variables TOKEN, DOMAIN, ID
+# loaded by .zshrc
+
+ACCESS_TOKEN=$TOKEN
+DOMAIN=$DOMAIN
+RECORD_IDS=($ID)	# multiple ids (RECORD_ID_1 RECORD_ID_n)
 
 IP=$(curl -s http://checkip.amazonaws.com/)
 
 echo $(date)
 echo "Current Public IP : $IP"
-echo "Updating Digital Ocean DNS record ... "
 
-for ID in "${RECORD_IDS[@]}"
-do
-  curl \
-    -fs -o /dev/null \
-    -X PUT \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $ACCESS_TOKEN" \
-    -d "{\"data\":\"$IP\"}" \
-    "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$ID"
-done
+if [[ $(< do-ddns/ip.txt) != "$IP" ]]; then
+	echo "Updating Digital Ocean DNS record ... "
+	for ID in "${RECORD_IDS[@]}"
+	do
+  		curl \
+    		-fs -o /dev/null \
+		-X PUT \
+		-H "Content-Type: application/json" \
+		-H "Authorization: Bearer $ACCESS_TOKEN" \
+		-d "{\"data\":\"$IP\"}" \
+		"https://api.digitalocean.com/v2/domains/$DOMAIN/records/$ID"
+	done
+	echo $IP > do-ddns/ip.txt
+else
+	echo "Public IP unchanged. Exiting ... "
+fi
 
 echo "** Done. **"
 exit
